0bedae6f902e2d8ac08a1d5d618d381bdc56fec0
MEGalamba <mariaeduardagalamba@gmail.com>
2025-12-28 23:16:27 +0000

    feat: added filter to foodstuff List

--- season-cook/src/components/Foodstuff/FoodstuffList.jsx
+++ season-cook/src/components/Foodstuff/FoodstuffList.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import { getFoodstuffs } from "../../services/foodstuffService";
-//import foodstuffs from "../data/foodstuffs";
+import SeasonFilterBar from "../ui/SeasonFilter";
+import SearchFilterBar from "../ui/SearchFilter";
 import FoodstuffCard from "./FoodstuffCard";
 
 function FoodstuffList() {
   const [foodstuffs, setFoodstuffs] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [season, setSeason] = useState("Todo o ano");
+  const [search, setSearch] = useState("");
 
   async function fecthFoodstuffs() {
     try {
@@ -22,11 +25,21 @@ function FoodstuffList() {
     fecthFoodstuffs();
   }, []);
 
+  //filtered Foods by season and by name
+  const filteredFoods = foodstuffs.filter((foodstuffs) => {
+    return (
+      (season === "Todo o ano" || foodstuffs.season === season) &&
+      foodstuffs.name.toLowerCase().includes(search.toLowerCase())
+    );
+  });
+
   if (!loading) {
     return (
       <div>
-        {foodstuffs.map((food) => (
-          <FoodstuffCard key={food.id} food={food}></FoodstuffCard>
+        <SeasonFilterBar seasonValue={season} onSeasonChange={setSeason} />
+        <SearchFilterBar searchValue={search} onSearchChange={setSearch} />
+        {filteredFoods.map((food) => (
+          <FoodstuffCard key={food.id} food={food} />
         ))}
       </div>
     );
