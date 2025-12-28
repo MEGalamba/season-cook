c26cb9a807fdda3973b005feacb5e81aa15bc7a9
MEGalamba <mariaeduardagalamba@gmail.com>
2025-12-28 18:22:35 +0000

    feat: data fecthed from firebase db

--- season-cook/src/components/Foodstuff/FoodstuffList.jsx
+++ season-cook/src/components/Foodstuff/FoodstuffList.jsx
@@ -1,15 +1,38 @@
-import foodstuffs from "../data/foodstuffs";
+import { useState, useEffect } from "react";
+import { getFoodstuffs } from "../../services/foodstuffService";
+//import foodstuffs from "../data/foodstuffs";
 import FoodstuffCard from "./FoodstuffCard";
 
 function FoodstuffList() {
-  return (
-    <div>
-      {foodstuffs.map((food) => (
-        <FoodstuffCard key={food.id} food={food}></FoodstuffCard>
-      ))}
-    </div>
-  );
+  const [foodstuffs, setFoodstuffs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function fecthFoodstuffs() {
+    try {
+      const data = await getFoodstuffs();
+      setFoodstuffs(data);
+    } catch (erro) {
+      console.log(erro);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fecthFoodstuffs();
+  }, []);
+
+  if (loading) {
+    return "";
+  } else {
+    return (
+      <div>
+        {foodstuffs.map((food) => (
+          <FoodstuffCard key={food.id} food={food}></FoodstuffCard>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default FoodstuffList;
-
