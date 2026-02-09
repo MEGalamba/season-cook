1b6a581372c54c1626372f4b2c10c4ae2e1f79e6
MEGalamba <mariaeduardagalamba@gmail.com>
2026-02-09 21:12:10 +0000

    minotr fixes

--- season-cook/src/pages/HomePage.jsx
+++ season-cook/src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
 //import BlogHighlights from "./BlogHighlights";
 //import Footer from "../../components/Footer";
-import Hero from "../../components/ui/Hero";
-import Foodstuff from "../Foodstuff";
-import Recipes from "../Recipes";
-import Section from "../../components/ui/Section";
+import Hero from "../components/ui/Hero";
+import Foodstuff from "./Foodstuff";
+import Recipes from "./Recipes";
+import Section from "../components/ui/Section";
 import { Container } from "react-bootstrap";
 
 function HomePage() {
